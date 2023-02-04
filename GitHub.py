a64139862466d24534d937ccece1abb7cb04fdfc
cotton-daddy <vmircevski@gmail.com>
2023-02-04 16:25:43 +0100

    Update GitHub.py

--- GitHub.py
+++ GitHub.py
@@ -4,22 +4,24 @@ import datetime
 
 # Get some stats from a rich public repository to help with a Personal Software Process presentation
 
+# GitHub authentication token
+token="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";
+
 def get_most_contributed_files(username:str, token:str):
     # Get the date one year ago
     now = datetime.datetime.now()
     last_year = (now - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
-    print(last_year)
     # Set the authentication headers
     headers = {"Authorization": f"Token {token}"}
     # Use the GitHub API to get the user's contributions
     url = f"https://api.github.com/search/commits?q=author:{username}+committer-date:>={last_year}"
     response = requests.get(url, headers=headers)
-    print(response.text)
     commits = json.loads(response.text)['items']
     # Get a list of files contributed to
     files = {}
     for commit in commits:
-        for file in commit.get("files"):
+        response = requests.get(f"https://api.github.com/repos/{commit.get('repository').get('full_name')}/commits/{commit.get('sha')}", headers=headers)
+        for file in json.loads(response.text)['files']:
             file_name = file['filename']
             if file_name not in files:
                 files[file_name] = 1
@@ -31,10 +33,5 @@ def get_most_contributed_files(username:str, token:str):
     print(f"The user {username} has contributed to the following files in the last year:")
     for i, file in enumerate(sorted_files[:10]):
         print(f"{i+1}. {file[0]} ({file[1]} contributions)")
-
-token="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";
-
-user_details = requests.get("https://api.github.com/user", headers={"Authorization": f"Token {token}"}).json()
-print(f"You are authenticated as {user_details['login']}")
 
 get_most_contributed_files("cotton-daddy", token)
